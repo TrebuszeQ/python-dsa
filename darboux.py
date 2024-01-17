8ef2dc22e4b71229490ff10fcec927e3194b2de7
TrebuszeQ <trebuszeq@gmail.com>
2024-01-17 03:36:50 +0100

    17.01.23 darbour counter

--- darboux.py
+++ darboux.py
@@ -1,4 +1,6 @@
 from cli import Cli
+
+
 class Darboux:
 
     # funkcja f(x) jest ciągła w przedziale <a,b>
@@ -6,22 +8,66 @@ class Darboux:
     # funkcja przyjmuje różne znaki na końcach przedziału: f(a)*f(b)<0
 
     @staticmethod
-    def find_zero(a, b, d, c):
+    def _take_range_float():
+        a = 0
+        b = 0
+
+        while a >= b:
+            a = Cli.try_read_input_float("Podaj wartosc dolna przedzialu [typu float].\n")
+            b = Cli.try_read_input_float("Podaj wartosc gorna przedzialu [typu float].\n")
+            if a >= b:
+                print("a nie moze byc wieksze badz rowne od b.")
+        return a, b
+
+    @staticmethod
+    def _find_zero_case_2(b):
+        return Darboux._find_zero_case_2(b)
+
+    @staticmethod
+    def _find_zero_case_1(a):
+        return Darboux._find_zero_case_1(a)
+
+    @staticmethod
+    def _find_zero(a, b, d, c):
         c += 1
-        x = (a + b)/2
-        f = x*x*x - x*x - x + 2
+        try:
+            x = (a + b) / 2
+            f = x * x * x - x * x - x + 2
 
-        if c == 800:
-            print("Funkcja nie ma pierwiastków w przedziale.")
-            return False;
+            if abs(f) <= d:
+                print(x, " z dokladnoscia ", d)
+                return True, c
 
-        if abs(f) <= d:
-            print(x, 'z dokładnością ', d)
-            return x
+            elif f * (a * a * a - a * a - a + 2) > 0:
+                return Darboux._find_zero(a, x, d, c)
 
-        elif ((a*a*a - a*a - a + 2) * f) < 0:
-            return Darboux.find_zero(a, x, d, c)
+            else:
+                print("Case 3")
+                return Darboux._find_zero(x, b, d, c)
 
-        else:
-            return Darboux.find_zero(x, b, d, c)
+        except RecursionError:
+            return False, c
+
+    @staticmethod
+    def darboux():
+        c = 0
+        i = 0
+
+        # here
+        res = (False, 0)
+
+        while not res:
+            a, b = Darboux._take_range_float()
+            d = Cli.try_read_input_float("Podaj dokladnosc [typu float].\n")
+
+            res = Darboux._find_zero(a, b, d, c)
+            i += 1
+            print(i + c)
+
+            if res is False:
+                print("Funkcja nie ma miejsc zerowych w przedziale.\n")
+
+        return res
+
+
 
--- darboux_test.py
+++ darboux_test.py
@@ -5,8 +5,17 @@ from darboux import Darboux
 
 
 class DarbouxTestCase(unittest.TestCase):
-    def test_find_zero(self):
-        print(Darboux.find_zero(2, 36, 1, 0))
+    # def test_find_zero(self):
+    #     print(Darboux._find_zero(2, 6, 1))
+
+    def test_find_zero_invalid(self):
+        self.assertFalse(Darboux._find_zero(2, 36, 1, 0))
+
+    def test_find_zero_valid(self):
+        self.assertTrue(Darboux._find_zero(2, 6, 6, 0))
+
+    def test_darboux_invalid(self):
+        Darboux.darboux()
 
 
 if __name__ == '__main__':
