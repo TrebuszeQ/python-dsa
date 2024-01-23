8649d7d2d3dcd31af6b14873b03fcef0b6de3314
TrebuszeQ <trebuszeq@gmail.com>
2024-01-23 13:17:20 +0100

    23.01.24 quicksort

--- sortings/quick_sort.py
+++ sortings/quick_sort.py
@@ -2,12 +2,37 @@
 
 class QuickSort:
     @staticmethod
-    def sort(arr, i, j):
-        pivot = QuickSort._pick_pivot_last(arr)
-        j = i - 1
-        if arr[i] < pivot and arr[j] > arr[i]:
-
+    def _pick_pivot_last(arr):
+        return arr[arr.__len__() - 1]
 
     @staticmethod
-    def _pick_pivot_last(arr):
-        return arr.__len__() - 1
+    def _sort(arr, i):
+
+        if arr[i + 1] is not None and arr[i] > arr[i + 1]:
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+            i += 1
+            return QuickSort._sort(arr, i)
+
+        elif arr[i + 1] is None:
+            return arr
+
+    @staticmethod
+    def sort(arr):
+        res = QuickSort._partition(QuickSort._pick_pivot_last(arr), arr)
+        res[0].extend(res[1])
+        print(res[0])
+        return res[0]
+
+    @staticmethod
+    def _partition(pivot, arr):
+        res = [[], []]
+
+        i = 0
+        while i < len(arr):
+            if arr[i] >= pivot:
+                res[1].append(arr[i])
+            else:
+                res[0].append(arr[i])
+            i += 1
+
+        return res
