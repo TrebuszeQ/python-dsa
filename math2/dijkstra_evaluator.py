689ca608f483d84b091e032824d932a9fba052c0
TrebuszeQ <trebuszeq@gmail.com>
2024-02-08 08:53:24 +0100

    08.02.2024 dijkstra expression evaluator

--- math2/dijkstra_evaluator.py
+++ math2/dijkstra_evaluator.py
@@ -24,51 +24,66 @@ class DijkstraEvaluator:
 
         for op in operations:
             if op["name"] == operator:
-                return op
+                return op["operation"]
 
     @staticmethod
-    def _trim_empty(text):
+    def _trim_whitespace(text):
         return text.replace(' ', '')
 
     @staticmethod
-    def _traverse():
+    def _char_to_number(char):
+        if char.isdigit():
+            return int(char)
+
+        return ord(char)
+
+    @staticmethod
+    def _take_expression():
+        return DijkstraEvaluator._trim_whitespace(Cli.try_read_input_string("Podaj wyrazenie arytmetyczne.\n"))
+
+    @staticmethod
+    def _traverse(expression):
         operators = Stack([])
         values = Stack([])
 
-        expression = DijkstraEvaluator._trim_empty(Cli.try_read_input_string("Podaj wyrazenie arytmetyczne.\n"))
-
         result = 0
-
+        precedense = False
         for char in expression:
             match char:
                 case '(':
                     operators.push('(')
 
                 case ')':
-                    while operators.peek() != '(':
+                    while operators.top() != '(' and values.size() > 0:
                         value2 = values.pop()
                         value1 = values.pop()
-                        result = DijkstraEvaluator._evaluate(operators.pop(), value1, value2)
+                        values.push(DijkstraEvaluator._evaluate(operators.pop(), value1, value2))
 
                     operators.pop()
-                    values.push(result)
 
-                case '-' | '+' | '*' | '/' | ':' | '%' | 'sqrt' | '^':
+                case '*' | '/' | ':' | '%' | 'sqrt' | '^':
+                    precedense = True
+                    operators.push(char)
+
+                case '-' | '+':
                     operators.push(char)
 
                 case _:
-                    if char.isalnum():
-                        values.push(char)
+                    if char.isalnum() and precedense is True:
+                        values.push(DijkstraEvaluator._evaluate(operators.pop(), values.pop(), DijkstraEvaluator._char_to_number(char)))
+                        precedense = False
 
-        print(operators)
-        print(values)
+                    if char.isalnum() and precedense is not True:
+                        values.push(DijkstraEvaluator._char_to_number(char))
+
         return result
 
     @staticmethod
     def main():
         print("Program oblicza wyrazenie arytmetyczne w nawiasach.\n")
+        return DijkstraEvaluator._traverse(DijkstraEvaluator._trim_whitespace(DijkstraEvaluator._take_expression()))
 
 
 if __name__ == '__main__':
-    DijkstraEvaluator._traverse()
+    DijkstraEvaluator.main()
 
--- math2/dijkstra_evaluator_test.py
+++ math2/dijkstra_evaluator_test.py
@@ -1,9 +1,11 @@
 import unittest
 
+from dijkstra_evaluator import DijkstraEvaluator
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_traverse_valid(self):
+        self.assertEqual(7.5, DijkstraEvaluator._traverse("(2 * 3 + (1 + 2) / 2)"))  # add assertion here
 
 
 if __name__ == '__main__':
