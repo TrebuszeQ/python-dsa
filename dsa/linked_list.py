0aacf38c41ce9c4155469e4ca8ad57cb8fd9368d
TrebuszeQ <trebuszeq@gmail.com>
2024-02-20 08:30:58 +0100

    linked list

--- dsa/linked_list.py
+++ dsa/linked_list.py
@@ -1,20 +1,34 @@
-class LinkedList:
-    class Node:
-        def __init__(self, data):
-            self.Node = data
-            self.next: LinkedList.Node | None = None
-
-        def __repr__(self):
-            return f'<{type(self).__name__} at 0x{id(self):x}>'
+class Node:
+    data = None
+    next = None
 
     def __init__(self, data):
-        self.head = self.Node(data)
+        self.data = data
+        self.next = None
 
     def __repr__(self):
+        return (f'<{type(self).__name__}\n'
+                f'self.data {self.data}\n'
+                f'self.next {self.next}\n'
+                f'at 0x{id(self):x}>')
+
+
+class LinkedList:
+    def __init__(self):
+        self.head: Node | None = None
+
+    def __repr__(self):
+        nodes = []
+        while self.head is not None:
+            nodes.append(self.head.data)
+            self.head = self.head.next
+
+        nodes.append('None')
+        " -> ".join(nodes)
         return f'<{type(self).__name__} at 0x{id(self):x}'
 
     def insert_head(self, data):
-        self.head = self.Node(data)
+        self.head, self.head.next = Node(data), self.head
 
 
 
--- dsa/linked_list_test.py
+++ dsa/linked_list_test.py
@@ -4,15 +4,25 @@ from linked_list import LinkedList
 
 
 class LinkedListTestCase(unittest.TestCase):
-    def test_linked_list(self):
-        linked_list = LinkedList(None)
-        self.assertEqual(type(linked_list), LinkedList)
+    def setUp(self):
+        self.linked_list = LinkedList()
 
-    def test_insert_head(self):
-        linked_list = LinkedList("paczek")
+    def test_node_valid(self):
+        self.assertEqual(LinkedList.Node, self.linked_list.Node)
+
+    def test_node_next(self):
+        self.assertEqual(None, self.linked_list.Node.next)
+
+    def test_linked_list_valid(self):
+        self.assertEqual(type(self.linked_list), LinkedList)
+
+    def test_insert_head_valid(self):
+        linked_list = LinkedList()
+        linked_list.insert_head("paczek")
         linked_list.insert_head("rurka")
-        print(linked_list.head.next)
-        self.assertEqual("paczek", linked_list.head)
+        print(linked_list.__repr__())
+        self.assertEqual("rurka", linked_list.head.data)
+        self.assertEqual("paczek", linked_list.head.next.data)
 
 
 if __name__ == '__main__':
