61d55d0fb2f8a42e5d3811fbbeaaf878320aec63
TrebuszeQ <trebuszeq@gmail.com>
2024-03-28 09:52:04 +0100

    single polynomial

--- app/mathematical/polynomials/single_polynomial.py
+++ app/mathematical/polynomials/single_polynomial.py
@@ -4,9 +4,9 @@ from dataclasses import dataclass
 
 @dataclass(repr=True)
 class SinglePolynomial:
-    @property
-    def n(self):
-        return self._n
+    def degree(self):
+        return self._degree
+
 
     @property
     def y(self):
@@ -22,36 +22,43 @@ class SinglePolynomial:
         self._y = p
 
     @property
+    def poly(self):
+        return self._poly
+
+    @poly.setter
+    def poly(self, arr: [[float]]):
+        self._poly = SinglePolynomial.__sort_poly(self._degree, arr)
+
+    @property
     def poly_str(self):
         return self._poly_str
 
     def __init__(self, poly_arr: [[float]]):
-        self._poly = poly_arr
-        self._poly = self.sort_poly()
-        self._n = SinglePolynomial.__find_max_coefficient(self)
+        self._degree = SinglePolynomial.__find_max_coefficient(poly_arr)
+        self._poly = SinglePolynomial.__sort_poly(self._degree, poly_arr)
         self._poly_str = SinglePolynomial.__poly_to_string(self)
         self._y = None
 
-    def sort_poly(self):
-        sorted_poly: [[float]] = None * self.n
+    @staticmethod
+    def __sort_poly(degree: float, arr: [[float]]):
+        sorted_poly: [[float]] = []
+        for i in range(round(int(degree))):
+            sorted_poly.append([0, 0])
 
-        i = self._n
+        i = degree
         while i != 0:
-            for pair in self._poly:
-                if pair[1] == i and sorted_poly[i] is None:
-                    sorted_poly[i] = pair
+            for pair in arr:
+                if pair[1] == i and sorted_poly.index(i) is None:
+                    sorted_poly.append(pair)
 
-                elif pair[1] == 1 and sorted_poly[i] is not None:
+                elif pair[1] == 1 and sorted_poly.index(i) is not None:
                     sorted_poly[i] += pair[0]
-
-            if sorted_poly[i] is None:
-                sorted_poly[i] = [0, 0]
 
         return sorted_poly
 
     def horner_method(self, x):
         poly = self._poly
-        n = self._n
+        n = self._degree
         p = None
         while n != 1:
             p = x * poly[n][0] + poly[n - 1][0]
@@ -68,11 +75,13 @@ class SinglePolynomial:
 
         return poly
 
-    def __find_max_coefficient(self):
+    # ?
+    @staticmethod
+    def __find_max_coefficient(arr: [[float]]):
         maxx = 0
-        for pair in self._poly:
+        for pair in arr:
             coefficient = pair[1]
-            if coefficient > self.n:
+            if coefficient > maxx:
                 maxx = coefficient
 
         return maxx
--- tests/mathematical/polynomials/single_polynomial_test.py
+++ tests/mathematical/polynomials/single_polynomial_test.py
@@ -1,23 +1,13 @@
-import unittest
 from app.mathematical.polynomials.single_polynomial import SinglePolynomial
+import unittest
 
 
 class MyTestCase(unittest.TestCase):
-
-    def test_poly_str_valid(self):
-        poly = SinglePolynomial([[-3.0, 3.0], [2.21, 2.0], [-2.5, 1.0]])
-        print(poly)
-        self.assertEqual("-3.0^3.0 +2.21^2.0 -2.5^1.0", poly.poly_str)
-
     def test_y_valid(self):
         poly_ar = [[-3.0, 3.0], [2.21, 2.0], [-2.5, 1.0]]
         poly = SinglePolynomial(poly_ar)
         poly.y = 1
         self.assertEqual(-24.6159, poly.y)
-
-    # def test_horner_method(self):
-    #     polynomial_dct: {float: float} = {1.0: 4.0, 3.0: 3.0, 1.0: 1.0, -3: 0}
-    #     self.assertEqual(-2, SinglePolynomial())
 
 
 if __name__ == '__main__':
