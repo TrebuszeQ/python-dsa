a43d90854800773b6b09462e82f33d73e0107983
TrebuszeQ <trebuszeq@gmail.com>
2024-04-23 10:36:06 +0200

    boolean algebra

--- app/algebra/boolean_algebra.py
+++ app/algebra/boolean_algebra.py
@@ -1,6 +1,22 @@
 
 
 class BooleanAlgebra:
+    @staticmethod
+    def _print_single_table_of_truth(p):
+        if p == 0:
+            truth_table_p = [1, 1, 0, 0]
+            truth_table_neg_p = [0, 0, 1, 1]
+
+        elif p == 1:
+            truth_table_p = [0, 0, 1, 1]
+            truth_table_neg_p = [1, 1, 0, 0]
+
+        res_table =  
+
+    @staticmethod
+    def _print_double_truth_table(p, q):
+        truth_table_p = [1, 1, 0, 0]
+        truth_table_q = [1, 0, 1, 0]
 
     @staticmethod
     def to_bin(num):
@@ -18,9 +34,14 @@ class BooleanAlgebra:
     #     return 1 - (1 - a0) * (1 - (1 - a0))
 
     @staticmethod
-    def excluded_middle(num):
-        return BooleanAlgebra.to_bin(num) or not BooleanAlgebra.to_bin(num)
+    def inversion(num):
+        return BooleanAlgebra.to_bin(~(~num))
 
     @staticmethod
     def double_negation(num):
-        return not (not BooleanAlgebra.to_bin(num))
+        p = int(BooleanAlgebra.to_bin(num)[0])
+        left = -(-p)
+        right = p
+        res = 1 - (left - right)**2
+        return
+
--- tests/algebra/boolean_algebra_test.py
+++ tests/algebra/boolean_algebra_test.py
@@ -3,13 +3,10 @@ from app.algebra.boolean_algebra import BooleanAlgebra
 
 
 class MyTestCase(unittest.TestCase):
-    def test_law_of_excluded_middle_valid(self):
-        binary = bin(5)[2:]
-        self.assertEqual(BooleanAlgebra.excluded_middle(5), binary)
 
     def test_law_of_double_negation_valid(self):
-        binary = bin(5)[2:]
-        self.assertEqual(BooleanAlgebra.double_negation(5), binary)
+        result = BooleanAlgebra.double_negation(3)
+        self.assertEqual(result, 1)
 
 
 if __name__ == '__main__':
