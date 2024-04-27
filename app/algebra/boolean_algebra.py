d9afcc8bcfcd31c02bc1bce0cbacb9fe15a2c56e
TrebuszeQ <trebuszeq@gmail.com>
2024-04-27 13:09:29 +0200

    boolean algebra

--- app/algebra/boolean_algebra.py
+++ app/algebra/boolean_algebra.py
@@ -4,125 +4,177 @@ from tabulate import tabulate
 
 class BooleanAlgebra:
 
-    @property
-    def p(self):
-        return self._p
+    @staticmethod
+    def make_table_of_truth(p, np, l, r, q=None, nq=None):
 
-    @property
-    def q(self):
-        return self._q
+        eq = 1 if l == r else 0
+        truth_table = [[1, np, p, l, r, eq], [2, p, np, l, r, eq]] if q is None \
+            else [[1, np, p, nq, q, l, r, eq], [2, p, np, nq, q, l, r, eq]]
 
-    @property
-    def res(self):
-        return self._res
-
-    @property
-    def truth_table(self):
-        return self._truth_table
-
-    def __init__(self, num):
-        self._p = 1 if num > 0 else 0
-        self._q = None
-        self._np = 1 - self._p
-        self._nq = None
-        self._res = None
-        self._truth_table = []
+        return truth_table
 
     @staticmethod
-    def _is_0_or_1(num):
-        return 1 if num > 0 else 0
-
-    def __set_table_of_truth(self, l, r):
-        eq = 1 if l == r else 0
-        self._truth_table = [[self._np, self._p, l, r, eq], [self._p, self._np, l, r, eq]] if self._q is None \
-            else [[self._np, self._p, self._nq, self._q, l, r, eq], [self._p, self._np, self._nq, self._q, l, r, eq]]
-
-    def print_table_of_truth(self, title="Truth Table"):
-        if self._q is None:
-            head = ["p", "~p", "l", "r", "l <=> r"]
-            indent = 34
+    def print_table_of_truth(truth_table, res, l_str, r_str, q=None, title="Truth Table"):
+        if q is None:
+            head = ["no.", "p", "~p", l_str, r_str, f"{l_str} <=> {r_str}"]
 
         else:
-            head = ["p", "~p", "q", "~q", "l", "r", "l <=> r"]
-            indent = 43
+            head = ["no.", "p", "~p", "q", "~q", l_str, r_str, f"{l_str} <=> {r_str}"]
 
         print(title)
-        print(tabulate(self._truth_table, headers=head, tablefmt="grid"))
-        print("".ljust(indent), self.res)
+        print(tabulate(truth_table, headers=head, tablefmt="grid"))
+        print(f"result: {res}")
+
         print()
 
-    def inversion(self):
-        return 1 - (1 - self._p)
+    @staticmethod
+    def inversion(p):
+        return 1 - (1 - p)
 
-    def double_negation(self, print_table: bool = True):
-        l = (1 - (1 - self._p))
-        r = self._p
-        self._res = 1 - (l - r)**2
-        self.__set_table_of_truth(l, r)
+    @staticmethod
+    def double_negation(x, print_table: bool = True):
+        p = 1 if x > 0 else 0
+        np = 0 if x > 1 else 1
+
+        l_str = "~(~p)"
+        l = (1 - (1 - p))
+
+        r_str = "p"
+        r = p
+
+        res = 1 - (l - r) ** 2
+        truth_table = BooleanAlgebra.make_table_of_truth(p, np, l, r)
+
         if print_table:
-            self.print_table_of_truth("Law of Double Negation")
-        return self._res
+            BooleanAlgebra.print_table_of_truth(truth_table, res, l_str, r_str, title="Law of Double Negation")
 
-    def excluded_middle(self, print_table: bool = True):
-        l = 1 - (1 - self._p) * (1 - (1 - self._p))
+        return res
+
+    @staticmethod
+    def excluded_middle(x, print_table: bool = True):
+        p = 1 if x > 0 else 0
+        np = 0 if x > 1 else 1
+
+        l_str = "[p | (~p)]"
+        l = 1 - (1 - p) * (1 - (1 - p))
+
+        r_str = "1"
         r = 1
-        self._res = (1 - (l - r) ** 2)
-        self.__set_table_of_truth(l, r)
-        if print_table:
-            self.print_table_of_truth("Law of Excluded Middle")
-        return self._res
 
-    def contradiction(self, print_table: bool = True):
-        l = (1 - (self._p & self._np))
-        r = 1
-        self._res = (1 - (l - r) ** 2)
-        self.__set_table_of_truth(l, r)
+        res = (1 - (l - r) ** 2)
+        truth_table = BooleanAlgebra.make_table_of_truth(p, np, l, r)
         if print_table:
-            self.print_table_of_truth("Law of Contradiction")
-        return self._res
+            BooleanAlgebra.print_table_of_truth(truth_table, res, l_str, r_str, title="Law of Excluded Middle")
+
+        return res
+
+    @staticmethod
+    def contradiction(x, print_table: bool = True):
+        p = 1 if x > 0 else 0
+        np = 0 if x > 1 else 1
+
+        l_str = "~(p & ~p)"
+        l = (1 - (p & np))
+
+        r_str = "1"
+        r = 1
+
+        res = (1 - (l - r) ** 2)
+        truth_table = BooleanAlgebra.make_table_of_truth(p, np, l, r)
+        if print_table:
+            BooleanAlgebra.print_table_of_truth(truth_table, res, l_str, r_str, title="Law of Contradiction")
+
+        return res
 
     # Prawo idempotentności alternatywy
-    def alternative_simplification(self, print_table: bool = True):
-        l = (self._p | self._p)
-        r = self._p
-        self._res = (1 - (l - r) ** 2)
-        self.__set_table_of_truth(l, r)
+    @staticmethod
+    def alternative_simplification(x, print_table: bool = True):
+        p = 1 if x > 0 else 0
+        np = 0 if x > 1 else 1
+
+        l_str = "(p | p)"
+        l = (p | p)
+
+        r_str = "p"
+        r = p
+
+        res = (1 - (l - r) ** 2)
+        truth_table = BooleanAlgebra.make_table_of_truth(p, np, l, r)
         if print_table:
-            self.print_table_of_truth("Law of Alternative Simplification")
-        return self._res
+            BooleanAlgebra.print_table_of_truth(truth_table, res, l_str, r_str, title="Law of Alternative Simplification")
 
-    def conjuction_simplification(self, print_table: bool = True):
-        l = (self._p & self._p)
-        r = self._p
-        self._res = (1 - (l - r) ** 2)
-        self.__set_table_of_truth(l, r)
+        return res
+
+    @staticmethod
+    def conjuction_simplification(x, print_table: bool = True):
+        p = 1 if x > 0 else 0
+        np = 0 if x > 1 else 1
+
+        l_str = "(p & p)"
+        l = (p & p)
+
+        r_str = "p"
+        r = p
+
+        res = (1 - (l - r) ** 2)
+        truth_table = BooleanAlgebra.make_table_of_truth(p, np, l, r)
         if print_table:
-            self.print_table_of_truth("Law of Conjuction Simplification")
-        return self._res
+            BooleanAlgebra.print_table_of_truth(truth_table, res, l_str, r_str, title="Law of Conjuction Simplification")
 
-    def first_clavius(self, print_table: bool = True):
-        l = self._p
-        r = self._np
-        self._res = (1 - (l - r) ** 2)
-        self.__set_table_of_truth(l, r)
+        return res
 
-        l2 = self._res
-        r2 = self._np
-        self._res = (1 - (l2 - r2) ** 2)
-        self.__set_table_of_truth(l2, r2)
+    @staticmethod
+    def first_law_of_clavius(x, print_table: bool = True):
+        p = 1 if x > 0 else 0
+        np = 0 if x > 1 else 1
+
+        l_str = "(p => ~p)"
+        l = 1 - (p & (1 - np))
+
+        r_str = "(~p)"
+        r = np
+
+        res = (1 - (l - r) ** 2)
+        truth_table = BooleanAlgebra.make_table_of_truth(p, np, l, r)
         if print_table:
-            self.print_table_of_truth("First Law of Clavius")
-        return self._res
+            BooleanAlgebra.print_table_of_truth(truth_table, res, l_str, r_str, title="First Law of Clavius")
 
-    def second_clavius(self, print_table: bool = True):
-        l = self._np
-        r = self._p
-        self._res = (1 - (l - r) ** 2)
+        return res
 
-        l2 = self._res
-        r2 = self._p
-        self._res = (1 - (l2 - r2) ** 2)
-        self.__set_table_of_truth(l2, r2)
+    @staticmethod
+    def second_law_of_clavius(x, print_table: bool = True):
+        p = 1 if x > 0 else 0
+        np = 0 if x > 1 else 1
+
+        l_str = "(~p => p)"
+        l = 1 - (np & (1 - p))
+
+        r_str = "p"
+        r = p
+
+        res = (1 - (l - r) ** 2)
+        truth_table = BooleanAlgebra.make_table_of_truth(p, np, l, r)
         if print_table:
-            self.print_table_of_truth("Second Law of Clavius")
-        return self._res
+            BooleanAlgebra.print_table_of_truth(truth_table, res, l_str, r_str, title="Second Law of Clavius")
+
+        return res
+
+    @staticmethod
+    def duns_scotus(x, y, print_table: bool = True):
+        p = 1 if x > 0 else 0
+        np = 0 if x > 1 else 1
+        q = 1 if y > 0 else 0
+        nq = 0 if y > 1 else 1
+
+        l_str = "(~q)"
+        l = 1 - q
+
+        r_str = "(q => p)"
+        r = 1 - (q & (1 - p))
+
+        res = 1 - l & (1 - r)
+        truth_table = BooleanAlgebra.make_table_of_truth(p, np, q, nq, l, r)
+        if print_table:
+            BooleanAlgebra.print_table_of_truth(truth_table, res, l_str, r_str, title="Law of Duns Scotus")
+
+        return res
--- tests/algebra/boolean_algebra_test.py
+++ tests/algebra/boolean_algebra_test.py
@@ -5,34 +5,36 @@ from app.algebra.boolean_algebra import BooleanAlgebra
 class MyTestCase(unittest.TestCase):
 
     def test_law_of_double_negation_valid(self):
-        algebra = BooleanAlgebra(3)
-        result = algebra.double_negation()
+        result = BooleanAlgebra.double_negation(3)
         self.assertEqual(result, 1)
 
     def test_law_of_excluded_middle_valid(self):
-        algebra = BooleanAlgebra(3)
-        result = algebra.excluded_middle()
+        result = BooleanAlgebra.excluded_middle(3)
         self.assertEqual(result, 1)
 
     def test_law_of_contradiction_valid(self):
-        algebra = BooleanAlgebra(3)
-        result = algebra.contradiction()
+        result = BooleanAlgebra.contradiction(3)
         self.assertEqual(result, 1)
 
     def test_law_of_alternative_simplification_valid(self):
-        algebra = BooleanAlgebra(3)
-        result = algebra.alternative_simplification()
+        result = BooleanAlgebra.alternative_simplification(3)
         self.assertEqual(result, 1)
 
     def test_law_of_conjuction_simplification_valid(self):
-        algebra = BooleanAlgebra(3)
-        result = algebra.conjuction_simplification()
+        result = BooleanAlgebra.conjuction_simplification(3)
         self.assertEqual(result, 1)
 
     def test_first_law_of_clavius_valid(self):
-        algebra = BooleanAlgebra(3)
-        result = algebra.first_clavius()
+        result = BooleanAlgebra.first_law_of_clavius(3)
         self.assertEqual(result, 1)
+
+    def test_second_law_of_clavius_valid(self):
+        result = BooleanAlgebra.second_law_of_clavius(3)
+        self.assertEqual(result, 1)
+
+    def test_law_of_duns_scotus(self):
+        result = BooleanAlgebra.duns_scotus(3, 4)
+        self.assertEqual(result, 0)
 
 
 if __name__ == '__main__':
