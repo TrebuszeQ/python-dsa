36e2716f97b79a768e5b8d32074f3e31ac5e3bea
TrebuszeQ <trebuszeq@gmail.com>
2024-05-15 20:32:16 +0200

    not even a commit

--- app/calculus/integrals/definite_integral.py
+++ app/calculus/integrals/definite_integral.py
@@ -17,18 +17,13 @@ class DefiniteIntegral:
 
     def compute_integral(self):
         h = (self._upper_bound - self._lower_bound) / self._num_intervals
-        integral_sum = 0
-        # a = self._function(self._upper_bound)
-        #
-        # for i in range(1, self._num_intervals):
-        #     b = self._function(self._lower_bound + h * i)
-        #     trap = Trapezium(a, b, h, self._plot_maker_service)
-        #     trap_area = trap.area
-        #
-        #     x_i = self._lower_bound + i * h
-        #     integral_sum += self.function(x_i) * h
+        sigma = 0
 
-        return integral_sum
+        for i in range(1, self._num_intervals + 1):
+            x = self._upper_bound + h * i
+            podstawa_b = self._function(x)
+
+        return sigma
 
     def __str__(self):
         return f"Definite integral of {self.function.__name__} from {self._lower_bound} to {self._upper_bound} to {self._upper_bound}."
