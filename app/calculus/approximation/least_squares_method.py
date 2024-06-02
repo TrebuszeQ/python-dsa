cfcd096a0fcf0226bce6083ed36f433821cded7b
TrebuszeQ <trebuszeq@gmail.com>
2024-06-02 19:57:55 +0200

    least something method done

--- app/calculus/approximation/least_squares_method.py
+++ app/calculus/approximation/least_squares_method.py
@@ -1,14 +1,31 @@
-def find_a(n, sigma_xy, sigma_x, sigma_y, sigma_x2):
-    a = (n * sigma_xy - sigma_x * sigma_y) / (n * sigma_x2 - (sigma_x ** 2))
+def find_a(n, sigma_xy, sigma_x, sigma_y, sigma_x2, debug=False):
+    denom = (n * sigma_xy - sigma_x * sigma_y)
+    meter = n * sigma_x2 - (sigma_x ** 2)
+    if debug:
+        print("a = (n * sigma_xy - sigma_x * sigma_y) / (n * sigma_x2 - (sigma_x ** 2))")
+        print(f"a = ({n} * {sigma_xy} - {sigma_x} * {sigma_y}) / ({n} * {sigma_x2} - {sigma_x ** 2})")
+        print(f"denom: {denom}; meter: {meter}")
+
+    a = denom / meter
+    if debug:
+        print(f"a: {a}\n")
     return a
 
 
-def find_b(n, a, sigma_y, sigma_x):
-    b = (1 / n) * (sigma_y - a * sigma_x)
+def find_b(n, a, sigma_y, sigma_x, debug=False):
+    denom = (1 / n)
+    meter = (sigma_y - a * sigma_x)
+    if debug:
+        print("b = (n * sigma_x - a) / (sigma_x * sigma_y)")
+        print(f"b = {(1 / n)} * ({sigma_y} - {a} * {sigma_x}))")
+        print(f"denom: {denom}; meter: {meter}")
+
+    b = denom * meter
+    if debug:
+        print(f"b: {b}\n")
     return b
 
-
-def least_squares_method(n, points_x, points_y, a=None, b=None):
+def least_squares_method(n, points_x, points_y, a=None, b=None, debug=False):
     x = points_x
     y = points_y
     sigma = 0
@@ -32,57 +49,85 @@ def least_squares_method(n, points_x, points_y, a=None, b=None):
         b = find_b(n, a, sigma_y, sigma_x)
 
     for i in range(n):
-        sigma += (y[i] - a * x[i] - b) ** 2
+        num = (y[i] - a * x[i] - b) ** 2
+        if debug:
+            print(f"no. {i}: sigma: {sigma} += ({y[i]} - {a} * {x[i]} - {b})^2 ; {num}")
+        sigma += num
 
+        if debug:
+            print(f"sigma least_squares_method: {sigma}\n")
     return sigma
 
 
-def find_sigma_xy(n, points_x, points_y):
+def find_sigma_xy(n, points_x, points_y, debug=False):
     x = points_x
     y = points_y
     sigma = 0
 
     for i in range(n):
-        sigma += (y[i] * x[i])
+        num = y[i] * x[i]
+        if debug:
+            print(f"no. {i}; sigma {sigma} += {y[i]} * {x[i]} ; {num} ")
+        sigma += num
 
+    if debug:
+        print(f"find_sigma_xy: {sigma}\n")
     return sigma
 
 
-def find_sigma_y(n, points_y):
+def find_sigma_y(n, points_y, debug=False):
     y = points_y
     sigma = 0
 
     for i in range(n):
+        if debug:
+            print(f"no. {i}; sigma {sigma} += {y[i]} ; {y[i]} ")
         sigma += y[i]
 
+    if debug:
+        print(f"find_sigma_y: {sigma}\n")
     return sigma
 
 
-def find_sigma_x(n, points_x):
-    x = points_x
-    sigma = 0
-
-    for i in range(n):
-        sigma += x[i]
-
-    return sigma
-
-
-def find_sigma_x2(n, points_x):
-    x = points_x
-    sigma = 0
-
-    for i in range(n):
-        sigma += x[i] ** 2
-
-    return sigma
-
-
-def find_sigma_y2(n, points_y):
+def find_sigma_y2(n, points_y, debug=False):
     y = points_y
     sigma = 0
 
     for i in range(n):
-        sigma += y[i] ** 2
+        num = y[i] ** 2
+        if debug:
+            print(f"no. {i}; sigma {sigma} += {y[i]} ** 2; {num}")
+        sigma += num
 
+    if debug:
+        print(f"find_sigma_y2: {sigma}\n")
+    return sigma
+
+
+def find_sigma_x(n, points_x, debug=False):
+    x = points_x
+    sigma = 0
+
+    for i in range(n):
+        if debug:
+            print(f"no. {i}; sigma {sigma} += {x[i]} ; {x[i]} ")
+        sigma += x[i]
+
+    if debug:
+        print(f"find_sigma_x: {sigma}\n")
+    return sigma
+
+
+def find_sigma_x2(n, points_x, debug=False):
+    x = points_x
+    sigma = 0
+
+    for i in range(n):
+        num = x[i] ** 2
+        if debug:
+            print(f"no. {i}; sigma {sigma} += {x[i]} ** 2; {num}")
+        sigma += x[i] ** 2
+
+    if debug:
+        print(f"find_sigma_x2: {sigma}\n")
     return sigma
--- tests/calculus/approximation/least_squares_method_test.py
+++ tests/calculus/approximation/least_squares_method_test.py
@@ -1,52 +1,56 @@
 import unittest
 import app.calculus.approximation.least_squares_method as lsm
-from math import floor
 
 
 class MyTestCase(unittest.TestCase):
-    def setUp(self):
+    def setUp1(self):
         self.points_x = self.x = [2, 4, 6, 8, 10]
-        self.points_y = self.y = [14, 27, 24, 44, 48]
+        self.points_y = self.y = [13, 27, 24, 44, 38]
         self.n = len(self.points_x)
 
     def test_find_a_valid(self):
-        sigma_xy = lsm.find_sigma_xy(self.n, self.x, self.y)
-        sigma_x = lsm.find_sigma_x(self.n, self.x)
-        sigma_y = lsm.find_sigma_y(self.n, self.y)
-        sigma_x2 = lsm.find_sigma_x2(self.n, self.x)
-        a = lsm.find_a(self.n, sigma_xy, sigma_x, sigma_y, sigma_x2)
-        self.assertEqual(4.25, a)
+        self.setUp1()
+        sigma_xy = lsm.find_sigma_xy(self.n, self.x, self.y, debug=True)
+        sigma_x = lsm.find_sigma_x(self.n, self.x, debug=True)
+        sigma_y = lsm.find_sigma_y(self.n, self.y, debug=True)
+        sigma_x2 = lsm.find_sigma_x2(self.n, self.x, debug=True)
+        a = lsm.find_a(self.n, sigma_xy, sigma_x, sigma_y, sigma_x2, debug=True)
+        self.assertAlmostEqual(3.35, a)
 
     def test_find_b_valid(self):
-        sigma_xy = lsm.find_sigma_xy(self.n, self.x, self.y)
-        sigma_x = lsm.find_sigma_x(self.n, self.x)
-        sigma_y = lsm.find_sigma_y(self.n, self.y)
-        sigma_x2 = lsm.find_sigma_x2(self.n, self.x)
-        a = lsm.find_a(self.n, sigma_xy, sigma_x, sigma_y, sigma_x2)
-        b = lsm.find_b(self.n, a, sigma_y, sigma_x)
-        self.assertEqual(5.9, b)  # add assertion here
+        self.setUp1()
+        sigma_xy = lsm.find_sigma_xy(self.n, self.x, self.y, debug=True)
+        sigma_x = lsm.find_sigma_x(self.n, self.x, debug=True)
+        sigma_y = lsm.find_sigma_y(self.n, self.y, debug=True)
+        sigma_x2 = lsm.find_sigma_x2(self.n, self.x, debug=True)
+        a = lsm.find_a(self.n, sigma_xy, sigma_x, sigma_y, sigma_x2, debug=True)
+        b = lsm.find_b(self.n, a, sigma_y, sigma_x, debug=True)
+        self.assertAlmostEqual(9.1, b)  # add assertion here
 
-    def setUp(self):
-        self.points_x = self.x = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0]
-        self.points_y = self.y = [5.0, 3.0, 6.0, 3.5, 4.5, 7.0, 5.5]
+
+    def setUp2(self):
+        self.points_x = self.x = [1.0, 2.0, 3.0, 4.0, 5.0]
+        self.points_y = self.y = [1.0, 5.0, 7.0, 12.0, 17.0]
         self.n = len(self.points_x)
 
     def test_find_a_valid2(self):
+        self.setUp2()
         sigma_xy = lsm.find_sigma_xy(self.n, self.x, self.y)
         sigma_x = lsm.find_sigma_x(self.n, self.x)
         sigma_y = lsm.find_sigma_y(self.n, self.y)
         sigma_x2 = lsm.find_sigma_x2(self.n, self.x)
         a = lsm.find_a(self.n, sigma_xy, sigma_x, sigma_y, sigma_x2)
-        self.assertEqual(4.25, a)
+        self.assertAlmostEqual(3.9, a)
 
     def test_find_b_valid2(self):
+        self.setUp2()
         sigma_xy = lsm.find_sigma_xy(self.n, self.x, self.y)
         sigma_x = lsm.find_sigma_x(self.n, self.x)
         sigma_y = lsm.find_sigma_y(self.n, self.y)
         sigma_x2 = lsm.find_sigma_x2(self.n, self.x)
         a = lsm.find_a(self.n, sigma_xy, sigma_x, sigma_y, sigma_x2)
         b = lsm.find_b(self.n, a, sigma_y, sigma_x)
-        self.assertEqual(5.9, b)  # add assertion here
+        self.assertAlmostEqual(-3.3, b)  # add assertion here
 
 
 if __name__ == '__main__':
